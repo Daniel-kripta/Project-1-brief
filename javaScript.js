b1eee79dcaa7a56c9098a2a6b863bdf1c5b5af37
Daniel-kripta <danielgz-profesional@outlook.es>
2026-04-09 17:44:34 +0100

    fixed menu issue & mode dark switch implemented

--- javaScript.js
+++ javaScript.js
@@ -4,7 +4,14 @@ class Headerjs extends HTMLElement {
         // Aquí dentro escribimos el HTML normal de la cabecera
         this.innerHTML = `
                     <header class="header-nav" id="top">
-                    <div class="logoArea"><a href="/"><img src="/assets/logos/circle.svg" alt="Web's logotype"></a>
+                    <div class="logoArea"><a href="/"><img class="CircleLogo" src="/assets/logos/circle.svg" alt="Web's logotype"></a>
+                    </div>
+
+                    <div id="themeMode">
+                        <label class="switch">
+                            <input name="Ligth-dark-mode" type="checkbox" id="toggleTheme">
+                            <span class="slider"></span>
+                        </label>
                     </div>
 
                     <nav class="centralNav">
@@ -13,7 +20,7 @@ class Headerjs extends HTMLElement {
                             <li><a href="/#Recent-Projects">Projects</a></li>
                             <li><a href="/#services">Services</a></li>
                             <li class="contactButtonSandwich blueButton"><a href="/pages/contact/">Contact Us</a></li>
-                    </nav>
+                   </ul> </nav>
 
                     <nav class="sandwichMenu"><button class="menuButton" id="sandwichMenu"><img id="iconMenu" src="/assets/icon/menu.svg" alt="Menú"></button></nav>
                     
@@ -21,7 +28,53 @@ class Headerjs extends HTMLElement {
 
                     </header>
                 `;
+                /*Mode dark switch*/
+
+        const themeModeCheck = this.querySelector("#toggleTheme");
+        const root = document.documentElement;
+
+
+        const savedThemeLS = localStorage.getItem("themeTogglePosition");
+
+        if (savedThemeLS === "1") {
+
+            themeModeCheck.checked = true;
+            root.style.setProperty('--color-darkblue', 'rgb(236, 247, 255)');
+            root.style.setProperty('--color-whitesky', 'rgb(41, 46, 71)');
+            root.style.setProperty('--color-blue', 'rgb(236, 247, 255)');
+            root.style.setProperty('--color-logo', 'brightness(40)');
+
+        }
+
+
+        themeModeCheck.addEventListener("change", function() {
+
+        const body = document.body;
+
+        body.classList.add('bodyTransicion');
+
+    const actualDarkBlue = getComputedStyle(root).getPropertyValue("--color-darkblue").trim();
+    const actualWhite = getComputedStyle(root).getPropertyValue("--color-whitesky").trim();
+    const actualBlue = getComputedStyle(root).getPropertyValue("--color-blue").trim();
+    const logoColor = getComputedStyle(root).getPropertyValue("--color-logo").trim();
+
+
+          
+    root.style.setProperty('--color-darkblue', actualDarkBlue === 'rgb(41, 46, 71)' ? 'rgb(236, 247, 255)' : 'rgb(41, 46, 71)');
+    root.style.setProperty('--color-whitesky', actualWhite === 'rgb(236, 247, 255)' ? 'rgb(41, 46, 71)' : 'rgb(236, 247, 255)');
+    root.style.setProperty('--color-blue', actualBlue === 'rgb(7, 42, 200)' ? 'rgb(236, 247, 255)' : 'rgb(7, 42, 200)'); 
+    root.style.setProperty('--color-logo', logoColor === 'none' ? 'brightness(40)' : 'none');
+
+    localStorage.setItem("themeTogglePosition", this.checked ? "1" : "0");
+    
+    setTimeout(() => {
+        body.classList.remove("bodyTransicion");
+    }, 3000);
+
+        });
+                
     }
+    
 }
 
 customElements.define("header-js", Headerjs);
@@ -29,11 +82,11 @@ customElements.define("header-js", Headerjs);
 // Footer con javascript para todas las páginas
 class Footerjs extends HTMLElement {
     connectedCallback() {
-        // Aquí dentro escribimos el HTML normal de la cabecera
+
         this.innerHTML = `
                     <footer>
                         <div class="address">
-                            <img src="/assets/logos/circle.svg" alt="Circle's logo">
+                            <img class="CircleLogo" src="/assets/logos/circle.svg" alt="Circle's logo">
                             <address>2972 Westheimer Rd. Santa Ana,<br>Illinois 85486</address>
                         </div>
                         <div class="footerLinks">
@@ -57,31 +110,52 @@ customElements.define("footer-js", Footerjs);
 // Section information con javascript para todas las páginas
 class Information extends HTMLElement {
     connectedCallback() {
-        // Aquí dentro escribimos el HTML normal de la cabecera
+
         this.innerHTML = `
                     <section class="client-information">
                         <div><h2>Do you have any questions?</h2>
                              <br>
                              <p>Let us help you!</p>
                         </div>
-                        <form>
-                            <div class="emailInputDiv"><img src="/assets/icon/mail.svg" class="emailIcon" alt=""><input type="email" placeholder="Enter you email" name="email" id="emailInput"></input>
-                            <button type="submit" class="submitButton">Suscribe</button>
+                        <form id="infoForm">
+                            <div class="emailInputDiv"><img src="/assets/icon/mail.svg" class="emailIcon" alt="">
+                            <input name="emailUser" type="email" placeholder="Enter you email" name="email" id="emailInput" required>
+                            </input>
+                            <button type="submit" class="submitButton">Ask for</button>
                             </div>
                         </form>
                     </section>
                 `;
+    const formInfo = document.getElementById("infoForm");
+
+    formInfo.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        const formData = new FormData(formInfo);
+        const email = formData.get("emailUser");
+        if (email) {
+            localStorage.setItem("userEmailTmp", email);
+            window.location.href = "/pages/contact";
+        }
+
+    });
     }
 }
 
 customElements.define("section-information", Information);
+
+// form information function
+
+
+
 
 
 // Captura de la API y declaración
 
 const API_URL = "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects";
 
-// Función asíncrona para obtener los artículos
+
 
 async function getAndDisplayAPI() {
     const projects = document.getElementById("projects");
@@ -175,8 +249,33 @@ document.getElementById("sandwichMenu").onclick = function() {
     }
         
     }
+/* Revert the menu functions */
 
-    /*form */
+    function closeMenu() {
+        const root = document.documentElement;
+        const body = document.body;
+        const menuIcon = document.getElementById("iconMenu");
+
+        root.style.setProperty('--menu-Sandwich-display', 'none');
+        root.style.setProperty('--menu-main-blured', 'none');
+        root.style.setProperty('--sroll-body-block', 'auto');
+
+        if (menuIcon.src.endsWith("menu_close.svg")) {
+        menuIcon.src = menuIcon.src.replace("menu_close.svg", "menu.svg");
+    }
+    }
+
+    const menuLinks = document.querySelectorAll("nav a"); 
+
+    menuLinks.forEach(link => {
+        link.onclick = function() {
+            closeMenu();
+        };
+    });
+
+
+
+    /*form help*/
             const form = document.getElementById("contactForm");
 
             form.addEventListener("submit", function(event) {
@@ -185,9 +284,6 @@ document.getElementById("sandwichMenu").onclick = function() {
 
             const formData = new FormData(form);
             const data = Object.fromEntries(formData.entries());
-
-            console.log("Form Submitted")
-            console.table(data);
 
             form.style.filter = "blur(5px)";
 
@@ -210,3 +306,18 @@ document.getElementById("sandwichMenu").onclick = function() {
             form.reset();
 
         });
+
+        document.addEventListener("DOMContentLoaded", () => {
+            const savedEmail = localStorage.getItem("userEmailTmp");
+
+            if (savedEmail) {
+                const emailInputDestiny = document.getElementById("email");
+
+                if (emailInputDestiny) {
+                    emailInputDestiny.value = savedEmail;
+
+                    localStorage.removeItem("userEmailTmp");
+                }
+            }
+        });
+
